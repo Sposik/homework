6f3f970f938e28d937ffb9a1f163a747db51db65
Spiridon Fes <mailboxpoker@gmail.com>
2025-03-04 18:16:09 +0300

    added get_date function

--- src/widget.py
+++ src/widget.py
@@ -19,7 +19,13 @@ def mask_account_card(number: str) -> str:
     full_alphas = " ".join(alphas)
     return f"{full_alphas} {get_mask_card_number(digits)}"
 
+def get_date(date: str) -> str:
+    split_date = date[:10].split("-")
+    wright_date = ".".join(reversed(split_date))
+    return wright_date
 
 
+
+print(get_date("2024-03-11T02:26:18.671407"))
 print(mask_account_card("Visa Classic 6831982476737658"))
 
