4e5e575d4b676fd39fcf1c896d223fdb5a72b548
Spiridon Fes <mailboxpoker@gmail.com>
2025-04-21 21:08:30 +0300

    logger added

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,41 @@
 from typing import Union
+import logging
 
+logger = logging.getLogger('masks')
+file_handler = logging.FileHandler('../Logs/masks.log', 'w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 def get_mask_card_number(number: Union[int, str]) -> str:
     """Функция маскировки номера банковской карты"""
+    logger.info('Старт функции маскировки номера карты')
     number = str(number).replace(" ", "")
     if not number.isdigit():
+        logger.error('Ошибка ввода данных карты')
         raise ValueError("Номер карты должен содержать только цифры")
     if len(number) != 16:
+        logger.error('Ошибка ввода данных карты')
         raise ValueError("Проверьте номер карты")
-    return f"{number[:4]} {number[4:6]} ** **** {number[-4:]}"
+    result = f"{number[:4]} {number[4:6]} ** **** {number[-4:]}"
+    logger.info(f'функция завершила работу с результатом {result}')
+    return result
 
 
 def get_mask_account(number: Union[int, str]) -> str:
     """Функция маскировки номера банковского счета"""
+    logger.info('Старт функции маскировки счета')
     number = str(number).replace(" ", "")
     if not number.isdigit():
+        logger.error('Ошибка ввода данных счета')
         raise ValueError("Номер счета должен содержать только цифры")
     if len(number) != 20:
+        logger.error('Ошибка ввода данных счета')
         raise ValueError("Проверьте номер счета")
-    return f"**{number[-4:]}"
+    result = f"**{number[-4:]}"
+    logger.info(f'функция завершила работу с результатом {result}')
+    return result
+
+print(get_mask_card_number(1234567890123456))
+print(get_mask_account(12345678901234567890))
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,25 @@
 import json
+import logging
 
+logger = logging.getLogger('utils')
+file_handler = logging.FileHandler('../Logs/utils.log', 'w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 def open_js(path):
     """Получение списка транзакций из файла"""
+    logger.info('Старт функции импорта')
     try:
         with open(path, "r", encoding="utf8") as f:
             data = json.load(f)
+        logger.info('Данные загружены')
         return data
-    except (FileNotFoundError, json.JSONDecodeError, TypeError, ValueError, KeyError):
+    except Exception as e:
+        logger.error(f'Произошла ошибка: {e}')
         return []
+    finally:
+        logger.info('Завершение работы')
+
+open_js('../Data/opertions.json')
