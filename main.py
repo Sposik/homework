d9d2b598b64965652df9333ab234bd3cc1e50b7c
Spiridon Fes <mailboxpoker@gmail.com>
2025-04-30 18:39:37 +0300

    13_2 complete

--- main.py
+++ main.py
@@ -1,29 +1,88 @@
-from src.generators import card_number_generator, filter_by_currency, transaction_descriptions, transactions
-from src.masks import get_mask_card_number
+from src.imports import import_csv, import_excel
 from src.processing import filter_by_state, sort_by_date
-from src.widget import mask_account_card
-
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+from src.utils import filter_rub_transactions, open_js, search_transactions_by_description
+from src.widget import get_date, mask_account_card
 
 
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(sort_by_date(data, reverse=False))
-print(filter_by_state(data, state="CANCELED"))
-print(get_mask_card_number("1234567890123456"))
+def main():
+    print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
+    print("Выберите необходимый пункт меню:")
+    print("1. Получить информацию о транзакциях из JSON-файла")
+    print("2. Получить информацию о транзакциях из CSV-файла")
+    print("3. Получить информацию о транзакциях из XLSX-файла")
+    while True:
+        choice_input = input("Пользователь: ").strip()
+        if choice_input == "1":
+            transactions = open_js("Data/operations.json")
+            print("Для обработки выбран Json файл")
+            break
+        elif choice_input == "2":
+            transactions = import_csv("Data/transactions.csv")
+            print("Для обработки выбран CSV файл")
+            break
+        elif choice_input == "3":
+            transactions = import_excel("Data/transactions_excel.xlsx")
+            print("Для обработки выбран Excel файл")
+            break
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите 1, 2 или 3.")
+    print("Введите статус, по которому необходимо выполнить фильтрацию.")
+    print("Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING")
+    available_statuses = ["EXECUTED", "CANCELED", "PENDING"]
 
-for card_number in card_number_generator(1, 3):
-    print(card_number)
+    while True:
+        choice = input("Пользователь: ").strip().upper()
+        if choice in available_statuses:
+            filtered_transactions = filter_by_state(transactions, choice)
+            break
+        else:
+            print(f'Статус операции "{choice}" недоступен.')
+            print("\nВведите статус, по которому необходимо выполнить фильтрацию.")
+            print("Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING")
+
+    print("Отсортировать операции по дате? Да / Нет")
+    choice_date = input("Пользователь: ").strip()
+    if choice_date.upper() == "ДА":
+        print("Отсортировать по возрастанию или по убыванию?")
+        choice_direction = input("Пользователь: ").strip()
+        if choice_direction.upper() == "ПО УБЫВАНИЮ":
+            filtered_transactions = sort_by_date(filtered_transactions)
+        elif choice_direction.upper() == "ПО ВОЗРАСТАНИЮ":
+            filtered_transactions = sort_by_date(filtered_transactions, reverse=False)
+
+    print("Выводить только рублевые транзакции? Да/Нет")
+    choice = input("Пользователь: ").strip().upper()
+    if choice == "ДА":
+        filtered_transactions = filter_rub_transactions(filtered_transactions)
+
+    print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+    choice = input("Пользователь: ").strip().upper()
+    if choice == "ДА":
+        print("Введите слово для фильтрации")
+        pattern = input("Пользователь: ").strip()
+        filtered_transactions = search_transactions_by_description(filtered_transactions, pattern)
+
+    print("Распечатываю итоговый список транзакций...\n")
+    print(f"Всего банковских операций в выборке: {len(filtered_transactions)}")
+    for transaction in filtered_transactions:
+        if choice_input == "1":
+            print(get_date(transaction.get("date")), " ", transaction.get("description"))
+            if transaction.get("description") == "Открытие вклада":
+                print(mask_account_card(transaction.get("to")))
+            else:
+                print(mask_account_card(transaction.get("from")), "->", mask_account_card(transaction.get("to")))
+                print(
+                    f"""Сумма: {transaction.get('operationAmount', {}).get('amount')}
+                    {transaction.get('operationAmount', {}).get('currency', {}).get('code')}"""
+                )
+        else:
+            print(get_date(transaction.get("date")), " ", transaction.get("description"))
+            if transaction.get("description") == "Открытие вклада":
+                print(mask_account_card(transaction.get("to")))
+            else:
+                print(mask_account_card(transaction.get("from")), "->", mask_account_card(transaction.get("to")))
+                print(f"Сумма: {transaction.get('amount')}," "{transaction.get('currency_code')}")
 
 
-descriptions = transaction_descriptions(transactions)
-for _ in range(5):
-    print(next(descriptions))
-
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import json
 import logging
+import re
+from collections import Counter
+from typing import Dict, List
 
 logger = logging.getLogger("utils")
-file_handler = logging.FileHandler("../Logs/utils.log", "w", encoding="utf-8")
+file_handler = logging.FileHandler("Logs/utils.log", "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -22,3 +25,60 @@ def open_js(path):
         return []
     finally:
         logger.info("Завершение работы")
+
+
+def search_transactions_by_description(transactions, search_pattern):
+    """
+    Ищет транзакции по заданному шаблону в описании операции
+    с использованием регулярных выражений
+    """
+    try:
+        pattern = re.compile(search_pattern, re.IGNORECASE)
+    except re.error:
+        raise ValueError("Некорректный шаблон регулярного выражения")
+
+    found_transactions = []
+
+    for transaction in transactions:
+        description = transaction.get("description", "")
+        if pattern.search(description):
+            found_transactions.append(transaction)
+
+    return found_transactions
+
+
+def count_transaction_types(transactions: List[Dict], category_counter: Dict[str, int] = None) -> Dict[str, int]:
+    """
+    Подсчитывает количество операций по категориям
+    """
+    # Инициализируем счетчик
+    counter = Counter(category_counter) if category_counter else Counter()
+
+    categories = (transaction.get("description", "Неизвестная операция").strip() for transaction in transactions)
+
+    counter.update(categories)
+
+    return dict(counter)
+
+
+def filter_rub_transactions(transactions: list) -> list:
+    """Фильтрует транзакции с валютой RUB"""
+    rub_transactions = []
+
+    for transaction in transactions:
+        # Для CSV данных
+        if "currency_code" in transaction:
+            currency = transaction.get("currency_code", "").upper()
+
+        # Для JSON данных
+        elif "operationAmount" in transaction:
+            currency = transaction.get("operationAmount", {}).get("currency", {}).get("code", "").upper()
+
+        # Для других форматов
+        else:
+            currency = transaction.get("currency", "").upper()
+
+        if currency == "RUB":
+            rub_transactions.append(transaction)
+
+    return rub_transactions
